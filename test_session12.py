c3c4765cf51d30f13ad91f2d06bf7253ee42b00d
sagar9926 <sagarkumar9926@gmail.com>
2020-10-27 21:26:35 +0530

    Create test_session12.py

---
+++ test_session12.py
@@ -0,0 +1,123 @@
+import inspect
+import math
+import os
+import re
+
+import pytest
+
+import calculator
+from calculator import derivatives
+
+# generic tests
+
+
+def test_readme_exists():
+    assert os.path.isfile("README.md"), "Put a README file, man!!"
+
+
+# def test_readme_contents():
+#     readme_words = [word for line in open("README.md", "r") for word in line]
+#     assert len(readme_words) >= 500, "Say more in the README. please....."
+
+
+# def test_readme_file_for_formatting():
+#     f = open("README.md", "r")
+#     content = f.rea_d()
+#     f.close()
+#     assert content.count("#") >= 10
+
+
+def test_indentations():
+    """ Returns pass if used four spaces for each level of syntactically \
+    significant indenting."""
+    lines = inspect.getsource(calculator)
+    spaces = re.findall("\n +.", lines)
+    for space in spaces:
+        assert len(space) % 4 == 2, "Your script contains misplaced indentations"
+        assert (
+            len(re.sub(r"[^ ]", "", space)) % 4 == 0
+        ), "Your code indentation does not follow PEP8 guidelines"
+
+
+def test_function_name_had_cap_letter():
+    functions = inspect.getmembers(calculator, inspect.isfunction)
+    for function in functions:
+        assert (
+            len(re.findall("([A-Z])", function[0])) == 0
+        ), "You have used Capital letter(s) in your function names"
+
+
+def test_function_has_annotations():
+    functions = inspect.getmembers(calculator, inspect.isfunction)
+    for _, function in functions:
+        assert (
+            function.__annotations__ is not {}
+        ), "You have to implement docstring for every function"
+
+
+def test_sin():
+
+    assert math.sin(3.14 // 2) == calculator.sin(3.14 // 2)
+
+
+def test_cos():
+    assert math.cos(3.14 // 2) == calculator.cos(3.14 // 2)
+
+
+def test_tan():
+    assert math.tan(3.14 // 2) == calculator.tan(3.14 // 2)
+
+
+def test_tanh():
+    assert math.tanh(3.14 // 2) == calculator.tanh(3.14 // 2)
+
+
+def test_log():
+    assert math.log(math.exp(1)) == calculator.log(math.exp(1))
+
+
+def test_exp():
+    assert math.exp(10) == calculator.exp(10)
+
+
+def test_sigmoid():
+    assert 1 / (1 + math.exp(-10)) == calculator.sigmoid(10)
+
+
+def test_relu():
+    assert max(0, -10) == calculator.relu(-10)
+
+
+def test_sin_d():
+
+    assert math.cos(3.14 // 2) == derivatives.sin_d(3.14 // 2)
+
+
+def test_cos_d():
+    assert -math.sin(3.14 // 2) == derivatives.cos_d(3.14 // 2)
+
+
+def test_tan_d():
+    assert 1-math.tan(3.14 // 2)**2 == derivatives.tan_d(3.14 // 2)
+
+
+def test_tanh_d():
+    assert 1-(math.tanh(3.14 // 2)**2) == derivatives.tanh_d(3.14 // 2)
+
+
+def test_log_d():
+    assert 1 == derivatives.log_d(1)
+
+
+def test_exp_d():
+    assert math.exp(10) == derivatives.exp_d(10)
+
+
+def test_sigmoid_d():
+    x=10
+    assert calculator.sigmoid(x)*(1-calculator.sigmoid(x)) == derivatives.sigmoid_d(x)
+
+
+def test_relu_d():
+    assert 0 == derivatives.relu_d(-10)
+    assert 1 == derivatives.relu_d(10000)
