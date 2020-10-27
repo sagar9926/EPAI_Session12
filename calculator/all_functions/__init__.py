383dff1d2a5d87bb237e0b18c8d0782e97ad72a8
sagar9926 <sagarkumar9926@gmail.com>
2020-10-27 21:14:56 +0530

    Update and rename f_sin.py to __init__.py

---
+++ calculator/all_functions/__init__.py
@@ -0,0 +1,12 @@
+from .cos import *
+from .exp import *
+from .log import *
+from .relu import *
+from .sigmoid import *
+from .tan import *
+from .tanh import *
+from .sin import *
+from .softmax import *
+
+__all__ = (cos.__all__ + exp.__all__ + log.__all__ + relu.__all__ + sigmoid.__all__ + sin.__all__ + 
+           tan.__all__ + softmax.__all__)
