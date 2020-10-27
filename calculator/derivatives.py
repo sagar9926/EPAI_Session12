1bf021de433e99af0a7c788ad3efa4c1aaf2a897
sagar9926 <sagarkumar9926@gmail.com>
2020-10-27 21:21:54 +0530

    Create derivatives.py

---
+++ calculator/derivatives.py
@@ -0,0 +1,9 @@
+from .all_functions.sin import f_dsin
+from .all_functions.log import f_dlog
+from .all_functions.exp import f_dexp
+from .all_functions.cos import f_dcos
+from .all_functions.tanh import f_dtanh
+from .all_functions.tan import f_dtan
+from .all_functions.sigmoid import f_dsigmoid
+from .all_functions.relu import f_drelu
+from .all_functions.softmax f_dsoftmax
