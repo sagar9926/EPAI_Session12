920ae7d592ee8a14cef20dea507f5b3ba5732d32
sagar9926 <sagarkumar9926@gmail.com>
2020-10-27 21:18:30 +0530

    Create tan.py

---
+++ calculator/all_functions/tan.py
@@ -0,0 +1,37 @@
+#tan.py
+
+import math
+from .f_string import f_string
+
+__all__ = ['f_tan']
+
+
+
+@f_string
+def f_tan(arg : float) -> 'Tangent of input argument':
+    """
+    Parameters
+    ----------
+    arg : Radian angle value in float
+
+    Returns
+    -------
+    Tangent of input argument
+
+    """
+    return(math.tan(arg))
+
+@f_string
+def f_dtan(arg : float) -> 'Derivative of Tangent of input argument' :
+    """
+    Parameters
+    ----------
+    arg : Radian angle value in float
+
+    Returns
+    -------
+    Derivative of Tangent of input argument
+
+    """
+
+    return(math.sec(arg)**2)
