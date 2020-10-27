16ad829bebaa38e840549a979e3181bfb25c78d8
sagar9926 <sagarkumar9926@gmail.com>
2020-10-27 21:16:37 +0530

    Create relu.py

---
+++ calculator/all_functions/relu.py
@@ -0,0 +1,39 @@
+#relu.py
+
+import math
+from .f_string import f_string
+
+__all__ = ['f_relu']
+
+
+
+@f_string
+def f_relu(arg : float) -> 'Applies RELU on input argument':
+    """
+    Parameters
+    ----------
+    arg : float
+
+    Returns
+    -------
+    Applies RELU on input argument
+
+    """
+    return(max(0,arg))
+
+@f_string
+def f_drelu(arg) -> 'Derivative of RELU of input argument':
+    """
+    Parameters
+    ----------
+    arg : float
+
+    Returns
+    -------
+    Derivative of RELU of input argument
+
+    """
+
+    return(1 if arg > 0  else 0)
+
+f_drelu(100)
